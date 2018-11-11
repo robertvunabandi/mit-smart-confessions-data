5e78058460bc1662ba00550b0f04656e66a7bd91
robertvunabandi <rvunabandi@gmail.com>
2018-11-11 01:06:17 -0500

    updating classification with metadata registering

--- models/classification-v2.py
+++ models/classification-v2.py
@@ -15,11 +15,14 @@ class BinaryClassification(BaseModel):
 		everything greater than cutoff as 1 and everything below
 		as 0.
 		"""
-		super(BinaryClassification, self).__init__("binary_classification")
+		super(BinaryClassification, self).__init__("bin_classification_i%d_c%d" % (fb_reaction_index, cutoff))
 		self.fb_reaction_index = fb_reaction_index
 		self.cutoff = cutoff
-		self.set_hyperparam(BaseModel.KEY_EPOCHS, 100)
+		self.register_metadata("fb_reaction_index")
+		self.register_metadata("cutoff")
+		self.set_hyperparam(BaseModel.KEY_EPOCHS, 150)
 		self.set_hyperparam(BaseModel.KEY_BATCH_SIZE, 128)
+		self.set_hyperparam(BaseModel.KEY_EMBEDDING_SIZE, 32)
 
 	def parse_base_data(
 		self,
@@ -35,7 +38,7 @@ class BinaryClassification(BaseModel):
 		embedding_size = self.get_hyperparam(BaseModel.KEY_EMBEDDING_SIZE)
 		model = keras.Sequential()
 		model.add(layers.Embedding(self.num_words, embedding_size, input_length=self.max_sequence_length))
-		model.add(layers.GlobalAveragePooling1D())
+		model.add(layers.Flatten())
 		model.add(layers.Dense(64, activation="relu"))
 		model.add(layers.Dropout(0.25))
 		model.add(layers.Dense(1, activation="sigmoid"))
@@ -46,15 +49,20 @@ class BinaryClassification(BaseModel):
 
 if __name__ == '__main__':
 	bc = BinaryClassification(data.data_util.FbReaction.LIKE_INDEX)
-	# bc.run(save=True)
-	bc.load()
-	d1 = bc.convert_text_to_padded_sequence("God damn it, I love you. Why do you have to be my ex's friend?")
-	d2 = bc.convert_text_to_padded_sequence("I'm a sophomore and I still don't know what Taylor series are.")
-	d3 = bc.convert_text_to_padded_sequence("I love MIT students")
-	d4 = bc.convert_text_to_padded_sequence("Hello there")
-	print(bc.predict(np.vstack((d1, d2, d3, d4))))
-	# got these after training and predicting
-	# [[0.00200981]  expected 0
-	#  [0.98490924]  expected 1
-	#  [0.20166883]
-	#  [0.49842522]]
+	bc.run(save=False)
+	# bc.load()
+	for t in [
+		"God damn it, I love you. Why do you have to be my ex's friend?",
+		"I'm a sophomore and I still don't know what Taylor series are.",
+		"I love MIT students",
+		"Hello there",
+	]:
+		d = bc.convert_text_to_padded_sequence(t)
+		print(t)
+		print(bc.predict(d))
+		print()
+		# got these after training and predicting
+		# [[0.02407354]  expected 0
+		#  [0.9994462]   expected 1
+		#  [0.8864635]
+		#  [0.5478533]]
