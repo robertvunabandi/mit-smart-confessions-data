2d9a5c93c0aba984beba377035e550df88b4b324
robertvunabandi <rvunabandi@gmail.com>
2018-11-20 21:57:52 -0500

    indenting with spaces now

--- models/parsing/tokenizer.py
+++ models/parsing/tokenizer.py
@@ -25,91 +25,91 @@ ADDITIONAL_WORDS = []
 
 
 def get_text_items(text_array: List[str]) -> Tuple:
-	"""
-	:param text_array : list[str]
-	:return Tokenizer
-	"""
-	tokenizer = Tokenizer(filters=FILTERS, split=" ")
-	tokenizer.fit_on_texts(list(text_array) + ["".join(ADDITIONAL_WORDS)])
-	index_to_word, word_to_index = _word_index_maps(tokenizer)
-	num_words = len(word_to_index)
-	sequences = _update_sequences_with_new_word_index_map(
-		tokenizer.texts_to_sequences(text_array),
-		tokenizer.index_word,
-		word_to_index,
-	)
-	return sequences, num_words, index_to_word, word_to_index
+    """
+    :param text_array : list[str]
+    :return Tokenizer
+    """
+    tokenizer = Tokenizer(filters=FILTERS, split=" ")
+    tokenizer.fit_on_texts(list(text_array) + ["".join(ADDITIONAL_WORDS)])
+    index_to_word, word_to_index = _word_index_maps(tokenizer)
+    num_words = len(word_to_index)
+    sequences = _update_sequences_with_new_word_index_map(
+            tokenizer.texts_to_sequences(text_array),
+            tokenizer.index_word,
+            word_to_index,
+    )
+    return sequences, num_words, index_to_word, word_to_index
 
 
 def split_dataset(
-	sequences: List[List[int]],
-	labels: List[int] or Tuple[int, ...],
-	word_to_index: Dict[str, int],
-	test_percentage: float = 0.1) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, int]:
-	"""
-	:param sequences : list[list[int]]
-	:param labels : list[int]|tuple[int]
-	:param word_to_index : dict<str, int>
-	:param test_percentage : float
-		of all the data, how many are used for the test set
-	:return : tuple<np.array, np.array, np.array, np.array, int>
-		-> training data, training labels, testing data, testing labels, max sequence length
-	"""
-	max_sequence_length = find_max_sentence_length(sequences)
-	split_index = len(sequences) - int(test_percentage * len(sequences))
-	raw_train_data, raw_train_labels = sequences[:split_index], list(labels[:split_index])
-	raw_test_data, raw_test_labels = sequences[split_index:], list(labels[split_index:])
-	train_data = pad_data_sequences(raw_train_data, word_to_index, max_sequence_length)
-	test_data = pad_data_sequences(raw_test_data, word_to_index, max_sequence_length)
-	if len(np.array(labels).shape) == 1:
-		train_labels = np.array([raw_train_labels]).T
-		test_labels = np.array([raw_test_labels]).T
-	else:
-		train_labels = np.array(raw_train_labels)
-		test_labels = np.array(raw_test_labels)
-	return train_data, train_labels, test_data, test_labels, max_sequence_length
+        sequences: List[List[int]],
+        labels: List[int] or Tuple[int, ...],
+        word_to_index: Dict[str, int],
+        test_percentage: float = 0.1) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, int]:
+    """
+    :param sequences : list[list[int]]
+    :param labels : list[int]|tuple[int]
+    :param word_to_index : dict<str, int>
+    :param test_percentage : float
+        of all the data, how many are used for the test set
+    :return : tuple<np.array, np.array, np.array, np.array, int>
+        -> training data, training labels, testing data, testing labels, max sequence length
+    """
+    max_sequence_length = find_max_sentence_length(sequences)
+    split_index = len(sequences) - int(test_percentage * len(sequences))
+    raw_train_data, raw_train_labels = sequences[:split_index], list(labels[:split_index])
+    raw_test_data, raw_test_labels = sequences[split_index:], list(labels[split_index:])
+    train_data = pad_data_sequences(raw_train_data, word_to_index, max_sequence_length)
+    test_data = pad_data_sequences(raw_test_data, word_to_index, max_sequence_length)
+    if len(np.array(labels).shape) == 1:
+        train_labels = np.array([raw_train_labels]).T
+        test_labels = np.array([raw_test_labels]).T
+    else:
+        train_labels = np.array(raw_train_labels)
+        test_labels = np.array(raw_test_labels)
+    return train_data, train_labels, test_data, test_labels, max_sequence_length
 
 
 def find_max_sentence_length(sequences: list) -> int:
-	"""
-	returns the longest sequence, which will be used as the maximum sequence.
-	:param sequences : list[list[int]]
-	:return int
-	"""
-	max_length = None
-	for seq in sequences:
-		if max_length is None or len(seq) > max_length:
-			max_length = len(seq)
-	return max_length
+    """
+    returns the longest sequence, which will be used as the maximum sequence.
+    :param sequences : list[list[int]]
+    :return int
+    """
+    max_length = None
+    for seq in sequences:
+        if max_length is None or len(seq) > max_length:
+            max_length = len(seq)
+    return max_length
 
 
 def pad_data_sequences(
-	sequences: List[List[int]],
-	word_to_index: Dict[str, int],
-	max_sequence_length: int,
-	padding: str = "post") -> np.ndarray:
-	return pad_sequences(
-		sequences, value=word_to_index[PAD], maxlen=max_sequence_length, padding=padding
-	)
+        sequences: List[List[int]],
+        word_to_index: Dict[str, int],
+        max_sequence_length: int,
+        padding: str = "post") -> np.ndarray:
+    return pad_sequences(
+            sequences, value=word_to_index[PAD], maxlen=max_sequence_length, padding=padding
+    )
 
 
 def convert_text_to_sequence(text: str, word_to_index: Dict[str, int]) -> list:
-	"""
-	:param text : str
-	:param word_to_index : dict<str, int>
-	:return list[int]
-	"""
-	words = utils.Str.extract_words_from_text_with_filters(text, FILTERS)
-	return [word_to_index.get(word, word_to_index[UNKNOWN]) for word in words]
+    """
+    :param text : str
+    :param word_to_index : dict<str, int>
+    :return list[int]
+    """
+    words = utils.Str.extract_words_from_text_with_filters(text, FILTERS)
+    return [word_to_index.get(word, word_to_index[UNKNOWN]) for word in words]
 
 
 def convert_sequence_to_text(sequence: List[int], index_to_word: Dict[int, str]) -> str:
-	"""
-	:param sequence : list[int]
-	:param index_to_word : dict<int, str>
-	:return str
-	"""
-	return " ".join([index_to_word.get(index, UNKNOWN) for index in sequence])
+    """
+    :param sequence : list[int]
+    :param index_to_word : dict<int, str>
+    :return str
+    """
+    return " ".join([index_to_word.get(index, UNKNOWN) for index in sequence])
 
 
 # ************************************
@@ -117,32 +117,32 @@ def convert_sequence_to_text(sequence: List[int], index_to_word: Dict[int, str])
 # ************************************
 
 def _word_index_maps(tokenizer: Tokenizer) -> Tuple[Dict[int, str], Dict[str, int]]:
-	"""
-	return dictionaries that can be used to map indices
-	to words and vice versa
-	:param tokenizer : Tokenizer
-	:return tuple<dict<int, str>, dict<str, int>>
-	"""
-	word_to_index = {k: (v + 4) for k, v in tokenizer.word_index.items()}
-	word_to_index[START] = 0
-	word_to_index[PAD] = 1
-	word_to_index[UNKNOWN] = 2
-	word_to_index[UNUSED] = 3
-	word_to_index[END] = 4
-	index_to_word = dict([(value, key) for (key, value) in word_to_index.items()])
-	return index_to_word, word_to_index
+    """
+    return dictionaries that can be used to map indices
+    to words and vice versa
+    :param tokenizer : Tokenizer
+    :return tuple<dict<int, str>, dict<str, int>>
+    """
+    word_to_index = {k: (v + 4) for k, v in tokenizer.word_index.items()}
+    word_to_index[START] = 0
+    word_to_index[PAD] = 1
+    word_to_index[UNKNOWN] = 2
+    word_to_index[UNUSED] = 3
+    word_to_index[END] = 4
+    index_to_word = dict([(value, key) for (key, value) in word_to_index.items()])
+    return index_to_word, word_to_index
 
 
 def _update_sequences_with_new_word_index_map(
-	raw_sequences: List[List[int]],
-	raw_index_to_word: Dict[int, str],
-	word_to_index: Dict[str, int]) -> List[List[int]]:
-	"""
-	Converts the raw sequences to a sequence with an updated word_to_index
-	dictionary. To do that, we need the original index_to_word, which is
-	given in raw_index_to_word.
-	"""
-	return [
-		[word_to_index[raw_index_to_word[index]] for index in sequence]
-		for sequence in raw_sequences
-	]
+        raw_sequences: List[List[int]],
+        raw_index_to_word: Dict[int, str],
+        word_to_index: Dict[str, int]) -> List[List[int]]:
+    """
+    Converts the raw sequences to a sequence with an updated word_to_index
+    dictionary. To do that, we need the original index_to_word, which is
+    given in raw_index_to_word.
+    """
+    return [
+        [word_to_index[raw_index_to_word[index]] for index in sequence]
+        for sequence in raw_sequences
+    ]
