e0ddd389fb55d3f13d3ec8ac48f4196e92d14ff6
robertvunabandi <rvunabandi@gmail.com>
2018-11-27 15:41:43 -0500

    adding the bucket frequencies into the metadata to store

--- models/bucket_classification.py
+++ models/bucket_classification.py
@@ -1,9 +1,11 @@
-import data.data_util
+from typing import List, Tuple, Union, Dict
+
 import numpy as np
 import keras
 from keras import layers
+
+import data.data_util
 from models.base_model import BaseModel
-from typing import List, Tuple, Union
 
 
 class BucketClassification(BaseModel):
@@ -45,9 +47,11 @@ class BucketClassification(BaseModel):
         self.fb_reaction_index = fb_reaction_index
         self.buckets = buckets
         self.bucket_ranges = None
+        self.bucket_frequencies = None
         self.register_metadata("fb_reaction_index")
         self.register_metadata("buckets")
         self.register_metadata("bucket_ranges")
+        self.register_metadata("bucket_frequencies")
         self.set_hyperparam(BaseModel.KEY_EPOCHS, 75)
         self.set_hyperparam(BaseModel.KEY_BATCH_SIZE, 64)
         self.set_hyperparam(BaseModel.KEY_EMBEDDING_SIZE, 32)
@@ -74,7 +78,8 @@ class BucketClassification(BaseModel):
         out = data.data_util.bucketize_labels(depol_labels, self.buckets)
         new_labels, self.bucket_ranges, b_frequencies = out
         freq_s = data.data_util.stringify_bucket_frequencies(b_frequencies)
-        print("parse_base_data::Bucket Frequencies::\n\n%s" % freq_s)
+        self.bucket_frequencies = BucketClassification.reformat_bucket_frequencies(b_frequencies)
+        print("parse_base_data::Bucket Frequencies::\n%s" % freq_s)
         padded = self.pad_sequences(depol_sequences)
         return padded, np.array(new_labels)
 
@@ -89,6 +94,16 @@ class BucketClassification(BaseModel):
         depol_sequences = [sequences[index] for index, _ in depol_label_with_indices]
         depol_labels = [labels[index] for index, _ in depol_label_with_indices]
         return depol_sequences, depol_labels
+
+    @staticmethod
+    def reformat_bucket_frequencies(
+            b_frequencies: Dict[Tuple[Union[int, float], Union[int, float]], int]
+    ) -> List[Tuple[Union[int, float], Union[int, float], int]]:
+        """
+        we need to reformat these because the keys are tuples, which is
+        not the JSON standard. This throws an error when trying to save.
+        """
+        return sorted([k + (v,) for k, v in b_frequencies.items()], key=lambda freq: freq[0])
 
     def create(self) -> None:
         embedding_size = self.get_hyperparam(BaseModel.KEY_EMBEDDING_SIZE)
