271963c630088f8bdf3b3bf42ced1697c6b6c439
robertvunabandi <rvunabandi@gmail.com>
2018-11-26 21:15:29 -0500

    a bit of a code refactoring to make it clearer. also some hyperparameter changed due to https://github.com/keras-team/keras/issues/2975

--- models/lstm_generator.py
+++ models/lstm_generator.py
@@ -1,13 +1,13 @@
-import data.data_util
 import numpy as np
 import keras
-from keras import layers, utils
+import keras.layers
+import keras.utils as ku
 from models.base_model import BaseModel
 import models.parsing.tokenizer
 from typing import List, Tuple, Union
 
+
 class LSTMGenerator(BaseModel):
-    #KEY_HIDDEN_NEURONS = "hidden_neurons"
     HIDDEN_NEURONS = 100
 
     def __init__(self):
@@ -18,75 +18,78 @@ class LSTMGenerator(BaseModel):
         as 0.
         """
         super(LSTMGenerator, self).__init__("lstm_generator")
-        self.set_hyperparam(BaseModel.KEY_EPOCHS, 100)
-        self.set_hyperparam(BaseModel.KEY_BATCH_SIZE, 128)
-        self.set_hyperparam(BaseModel.KEY_EMBEDDING_SIZE, 16)
-        self.set_hyperparam(BaseModel.KEY_TEST_SPLIT, 0)
-        self.set_hyperparam(BaseModel.KEY_VALIDATION_SPLIT, 0)
-        #self.set_hyperparam(LSTMGenerator.KEY_HIDDEN_NEURONS, 100)
-
+        self.set_hyperparam(BaseModel.KEY_EPOCHS, 100)  # ~10m/epoch => ~16.67hrs
+        self.set_hyperparam(BaseModel.KEY_BATCH_SIZE, 32)
+        self.set_hyperparam(BaseModel.KEY_EMBEDDING_SIZE, 300)
+        self.set_hyperparam(BaseModel.KEY_TEST_SPLIT, 0.0)
+        self.set_hyperparam(BaseModel.KEY_VALIDATION_SPLIT, 0.05)
 
     def create(self) -> None:
         embedding_size = self.get_hyperparam(BaseModel.KEY_EMBEDDING_SIZE)
-        hidden_neurons = LSTMGenerator.HIDDEN_NEURONS
-
-        input_len = self.max_sequence_length - 1
         model = keras.Sequential()
-        model.add(layers.Embedding(self.num_words, embedding_size, input_length=input_len))
-        model.add(layers.LSTM(hidden_neurons))
-        model.add(layers.Dense(self.num_words, activation='softmax'))
-        model.compile(loss='categorical_crossentropy', optimizer='adam')
+        model.add(keras.layers.Embedding(
+                self.num_words,
+                embedding_size,
+                embeddings_initializer=keras.initializers.RandomUniform(minval=-0.05, maxval=0.05, seed=None),
+                input_length=self.max_sequence_length - 1),
+        )
+        model.add(keras.layers.LSTM(LSTMGenerator.HIDDEN_NEURONS))
+        model.add(keras.layers.Dense(self.num_words, activation="softmax"))
+        model.compile(loss="categorical_crossentropy", optimizer="adamax", metrics=["accuracy"])
+        model.summary()
         self.model = model
 
-
-    def parse_base_data(self, sequences: List[List[int]],
-        labels: Tuple[Union[int, float], ...]) -> Tuple[np.ndarray, np.ndarray]:
-
-        input_sequences = []
-        for line in sequences:
-            for i in range(1, len(line)):
-                n_gram_sequence = line[:i+1]
-                input_sequences.append(n_gram_sequence)
-
-
+    def parse_base_data(
+            self,
+            sequences: List[List[int]],
+            labels: Tuple[Union[int, float], ...]) -> Tuple[np.ndarray, np.ndarray]:
+        """ create new sequences (from subsequences) then create labels with them """
+        input_sequences = LSTMGenerator.get_lstm_sequences(sequences)
         padded = self.pad_sequences(input_sequences, padding="pre")
-        train_data, new_labels = padded[:,:-1],padded[:,-1]
-
-        print(train_data)
-        print(new_labels)
-
-        new_labels = utils.to_categorical(new_labels, num_classes=self.num_words)
-
+        train_data, new_labels = padded[:, :-1], padded[:, -1]
+        new_labels = ku.to_categorical(new_labels, num_classes=self.num_words)
         return train_data, new_labels
 
+    @staticmethod
+    def get_lstm_sequences(sequences: List[List[int]]) -> List[List[int]]:
+        """
+        LSTMs need to always predict the next word, so we use this to cut each
+        sequence into its sub-sequences so that our LSTM is able to learn
+        faster.
+        Ideally, we'd do this for all sub-sequences of length at least 2.
+        However, that uses too much memory. So, instead we just do subsequences
+        from index 0 till index 2-len(sequence) for each sequence
+        """
+        new_sequences = []
+        for line in sequences:
+            for i in range(2, len(line)):
+                new_sequences.append(line[:i])
+        print("done: creating LSTM %d sequences" % len(new_sequences))
+        return new_sequences
 
     def generate(self, seed_text: str, total_word_additions: int):
-        #output_sequence = self.convert_text_to_padded_sequence(seed_text)
-        #initial_length = len(output_sequence)
         output_text = seed_text
+        sequence_len = self.max_sequence_length - 1
         for index in range(total_word_additions):
-            output_sequence = models.parsing.tokenizer.convert_text_to_sequence(output_text, self.word_to_index)
-            padded_sequence = self.pad_sequences([output_sequence], maxlen=self.max_sequence_length - 1, padding="pre")
-            predicted_index = self.model.predict_classes(padded_sequence, verbose=1)[0]
+            output_sequence = models.parsing.tokenizer.convert_text_to_sequence(
+                    output_text,
+                    self.word_to_index
+            )
+            padded_sequence = self.pad_sequences(
+                    [output_sequence[-sequence_len:]],
+                    maximum_sequence_len=sequence_len,
+                    padding="pre"
+            )
+            # todo - pick randomly among the top 5 so that predictions are different every time
+            predicted_index = self.model.predict_classes(padded_sequence, verbose=0)[0]
             output_text += " " + self.index_to_word[predicted_index]
-            #output_sequence.append(predicted_index)
-            #output_sequence = np.append(output_sequence, predicted_index)
-            #padded_sequence[0, index] = predicted_index
-        #index_to_word = dict([(index, word) for word, index in self.word_to_index.items()])
-        #return models.parsing.tokenizer.convert_sequence_to_text(output_sequence, self.index_to_word)
         return output_text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     generator = LSTMGenerator()
     generator.run(save=True)
-    #generator.load()
+    # generator.load()
     examples = [("Anime girls are better than real girls who is with us anime lovers", 20)]
-    for seed_text, total_word_additions in examples:
-        print(generator.generate(seed_text, total_word_additions))
-
-
-
-
-
-
+    for seed_text_, word_additions in examples:
+        print(generator.generate(seed_text_, word_additions))
